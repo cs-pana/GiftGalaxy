b818a3b184b7d50725164e610f603825c148723d
Elisa Ferrari <eli.fe.99@hotmail.it>
2024-08-30 15:13:06 +0200

    creazione scheletro progetto

---
+++ giftgalaxy/src/test/java/com/example/giftgalaxy/GiftgalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giftgalaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftgalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
